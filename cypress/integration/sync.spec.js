c41157b7031322b28ff67f82b3b616e50cd7d643
Lucas de Sousa Assunçao <lucasdesousa19@hotmail.com>
2021-05-18 10:17:16 -0300

    21 - wai e timeout

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -17,11 +17,31 @@ describe('Esperas ... ', () => {
             .type('Funcionou')
     })
 
-    it.only('Deve fazer retrys', () => {
+    it('Deve fazer retrys', () => {
         cy.get('#buttonDelay').click()
         cy.get('#novoCampo').should('not.exist')
         cy.get('#novoCampo')
             .should('exist')
             .type('funciona')
     })
+
+    it('Uso do find', () => {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
+    })
+
+    it.only('Uso do timeout', () => {
+        // cy.get('#buttonDom').click()
+        // cy.get('#novoCampo', { timeout: 1000 }).should('exist')
+
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li span', { timeout: 10000 })
+            .should('have.length', 2)
+    })
+
 })
